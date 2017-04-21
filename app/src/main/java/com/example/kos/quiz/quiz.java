eca605618ef97706fb001add0205a207f2d2a9cc
runekill <larsen@bk.ru>
2017-04-21 11:54:47 +0300

    First commit

---
+++ app/src/main/java/com/example/kos/quiz/quiz.java
@@ -0,0 +1,13 @@
+package com.example.kos.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);//kljlkjlkjkjhkjh
+    }
+}
