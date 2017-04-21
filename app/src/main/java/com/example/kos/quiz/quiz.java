4fab637fbf57d5053133ae73c3d87fe42a20689a
runekill <larsen@bk.ru>
2017-04-21 16:24:08 +0300

    Question and Next button
    Questions are separated into array, next button has been added.

--- app/src/main/java/com/example/kos/quiz/quiz.java
+++ app/src/main/java/com/example/kos/quiz/quiz.java
@@ -5,17 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class quiz extends AppCompatActivity {
 
     private Button mTtueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_cities, true),
+            new Question(R.string.question_oceans, false),
+    };
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         mTtueButton = (Button) findViewById(R.id.true_button);
         mTtueButton.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +42,14 @@ public class quiz extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(quiz.this, R.string.incorrect_toast,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
             }
         });
     }
